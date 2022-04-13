2b6aadd9f45c03a31f1241d9c17c9ba6621ebf85
Maicon Romano <shardersfx@gmail.com>
2022-04-12 21:47:57 -0300

    first commit

---
+++ aula5.py
@@ -0,0 +1,38 @@
+lista = [12, 10 , 7, 5]
+lista_animal = ['cachorro', 'gato', 'elefante',  'lobo', 'arara']
+# print(lista_animal [1])
+
+lista_animal[0] = 'macaco'
+print(lista_animal)
+
+tupla = (1, 10, 12, 14 )
+print(len(tupla))
+print(len(lista_animal))
+tupla_animal = tuple(lista_animal)
+print(type(tupla_animal))
+print(tupla_animal)
+lista_numerica = list(tupla)
+print(type(lista_numerica))
+lista_numerica[0] = 100
+print(lista_numerica)
+
+
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# lista_animal.reverse()
+# print(lista_animal)
+
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('não existe um lobo na lista. Será Incluído')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+# lista_animal.pop(1)
+# print(lista_animal)
+
+# lista_animal.remove('elefante')
+# print(lista_animal)
